a4b5662a09f25f8c6f6bdde4c1abc4b243ac8b59
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-22 22:02:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, render_template
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.route('/output')
-#def hello():
-    #test = 1+1
-    #return render_template('output.html', entry=output)
-
-print('hello')
+@app.route('/output')
+def hello():
+    test1 = float(1)
+    test2 = float(2)
+    test3 = test1 + test2
+    return test3
