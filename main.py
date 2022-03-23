6ebfda9b07d692656e89ca195dcb6616d7c85ff0
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 09:19:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    x = np.sum([1,2,3])
+    x = np.array([1,2,3])
     a = int(1)
     b = int(2)
     c = a + b
     y = str(x)
     test = str(c)
-    return x
+    return y
