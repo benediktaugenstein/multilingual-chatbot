f3b21cf3ba2cb0bab81988ae71126c7431ecf849
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 11:25:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import os
 import sys
 import numpy as np
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 def helper(x):
     return x+1
 
-@app.route('/')
-def hello():
-    test = input('Hier input')
-    #print('Hello', file=sys.stdout)
-    test2 = str(test)
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+    if request.method == "POST":
+       inp = request.form.get("inp") # name="inp" in html
+       return "Your input is "+inp
+    return render_template("output.html")
     return test2
