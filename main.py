433456975ffe4bbd09a2e91755e58d05b78b51b5
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 11:14:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def helper(x):
+    return x+1
+
 @app.route('/')
 def hello():
-    x = np.array([1,2,3])
-    a = int(1)
-    b = int(2)
-    c = a + b
-    y = str(x)
+    test = helper(1)
     print('Hello', file=sys.stdout)
-    test = str(c)
-    return y
+    return str(x)
