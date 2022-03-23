032563a908c9a197bf3586d7e7d9304ee339e2e2
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 16:09:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,23 +18,20 @@ def helper():
 
 @app.route('/')
 def my_form():
-    return render_template('input.html')
-
-@app.route('/', methods=['POST'])
-#@app.route('/')
-def output():
-    
     if not 'counter' in session:
         session['counter'] = 0
     session['counter'] += 1
     test_var_int = session['counter']
     test_var = str(test_var_int)
-    
+    return render_template('input.html')
+
+@app.route('/', methods=['POST'])
+#@app.route('/')
+def output():
     text = request.form['text']
     processed_text = helper2(text)
-    
     result = test_var + processed_text
     return render_template("input.html",result = result)
 
 if __name__ == '__main__':
-    app.run(threaded=False, processes=3)
+    app.run()
