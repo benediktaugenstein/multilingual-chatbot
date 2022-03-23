beff7b787681aa59a4957ac927d21a74f4afa795
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 08:32:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, render_template
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    x = np.array([1,2,3])
     a = int(1)
     b = int(2)
     c = a + b
+    y = str(x)
     test = str(c)
-    return test
+    return y
