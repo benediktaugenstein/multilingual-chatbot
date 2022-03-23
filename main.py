46329dc4c00bb72b489cf97c2f9fe90069b27f32
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 08:50:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import numpy as np
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    #x = np.sum([1,2,3])
+    x = np.sum([1,2,3])
     a = int(1)
     b = int(2)
     c = a + b
