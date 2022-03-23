427c6bf64ee9fa96cda9c82506dda90ba2d90b64
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 13:50:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def helper():
-    #x=x.upper()
-    return 'x'
+def helper(x):
+    x=x.upper()
+    return x
 
 @app.route('/')
 def my_form():
@@ -17,6 +17,6 @@ def my_form():
 #@app.route('/')
 def output():
     text = request.form['text']
-    processed_text = helper()
+    processed_text = helper(text)
     result = processed_text
     return render_template("input.html",result = result)
