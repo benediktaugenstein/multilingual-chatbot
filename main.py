194b01ffc9439fe17c57ebb4835c04614a25923c
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 13:33:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,15 @@ app = Flask(__name__)
 def helper(x):
     print(x)
 
-@app.route('/')
-def my_form():
-    return render_template('input.html')
+#@app.route('/')
+#def my_form():
+    #return render_template('input.html')
 
+#@app.route('/', methods=['POST'])
 @app.route('/', methods=['POST'])
 def output():
-    helper('hallo')
-    text = request.form['text']
-    processed_text = text.upper()
-    if result is not None:
-        pass
-    else:
-        result = ''
-    result = result + processed_text
-    return render_template("input.html",result = result)
+    return helper('test')
+    #text = request.form['text']
+    #processed_text = text.upper()
+    #result = result + processed_text
+    #return render_template("input.html",result = result)
