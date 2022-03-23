af44099d8aa3e37b452905f73da94c05dba9651e
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-03-23 15:22:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import sys
 import numpy as np
 from myfuncs import helper2
 from flask import Flask, render_template, request
-from dotenv import load_dotenv
-load_dotenv()
 
 app = Flask(__name__)
-app.run(threaded=True)
 
 test_var = '0'
 y = 'test'
@@ -31,3 +28,6 @@ def output():
     more_text = helper()
     result = more_text + processed_text
     return render_template("input.html",result = result)
+
+if __name__ == '__main__':
+    app.run()
