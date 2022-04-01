3cfa25998f98cc884235b77d14e6a6945f58afae
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-04-01 15:44:01 +0200

    Update myfuncs.py

--- myfuncs.py
+++ myfuncs.py
@@ -3,6 +3,18 @@ def helper2(x):
   return x
 
 def new_input(input_initial):
+  model = keras.models.load_model('/model/general')
+  model2 = keras.models.load_model('/model/feeling')
+
+  #link_general = '/data/general.csv'
+  #link_feeling = '/data/feeling.csv'
+  models = [model, model2]
+  datasets = [link_general, link_feeling]
+  tokenizers = []
+  word_indices = []
+  sequences_array = []
+  lengths_input = []
+  labels_transformed_array = []
   #input_initial = input('input here: ')
   #for word in inp.split():
     #if word.lower() in contractions:
