0a4f5b538582c885b8f0be9e6c9f60a394875ead
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-04-01 16:04:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,17 @@ def my_form():
 @app.route('/', methods=['POST'])
 #@app.route('/')
 def output():
+    model = keras.models.load_model('model/general')
+    model2 = keras.models.load_model('model/feeling')
+    #link_general = '/data/general.csv'
+    #link_feeling = '/data/feeling.csv'
+    models = [model, model2]
+    #datasets = [link_general, link_feeling]
+    tokenizers = []
+    word_indices = []
+    sequences_array = []
+    lengths_input = []
+    labels_transformed_array = []
     """
     if 'counter' in session:
         session['counter'] += 1
