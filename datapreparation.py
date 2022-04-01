d6b12fb6bd8a004de50021317b7fd0d338c8d6f8
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-04-01 15:13:55 +0200

    Update datapreparation.py

--- datapreparation.py
+++ datapreparation.py
@@ -1,1 +1,12 @@
+model = keras.models.load_model('/model/general')
+model2 = keras.models.load_model('/model/feeling')
 
+#link_general = '/data/general.csv'
+#link_feeling = '/data/feeling.csv'
+models = [model, model2]
+datasets = [link_general, link_feeling]
+tokenizers = []
+word_indices = []
+sequences_array = []
+lengths_input = []
+labels_transformed_array = []
