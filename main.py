fd825cd007c0ca7198c25380c3c42a48df20a106
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-04-01 16:53:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,45 @@ from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
 model = keras.models.load_model('model/general/general.h5')
-#model2 = keras.models.load_model('model/feeling')
-#link_general = '/data/general.csv'
-#link_feeling = '/data/feeling.csv'
-#models = [model, model2]
-#datasets = [link_general, link_feeling]
+model2 = keras.models.load_model('model/feeling/feeling.h5')
+link_general = '/data/general.csv'
+link_feeling = '/data/feeling.csv'
+models = [model, model2]
+datasets = [link_general, link_feeling]
 tokenizers = []
 word_indices = []
 sequences_array = []
 lengths_input = []
 labels_transformed_array = []
+
+for i, model in enumerate(models):
+  data = pd.read_csv(datasets[i])
+
+  sentences = data['Sentence']
+  labels = data['Result']
+
+  #tokenizer = Tokenizer(num_words = 100)
+  tokenizer = Tokenizer()
+  tokenizer.fit_on_texts(sentences)
+  word_index = tokenizer.word_index
+  sequences = tokenizer.texts_to_sequences(sentences)
+
+  tokenizers.append(tokenizer)
+  word_indices.append(word_index)
+
+  sequences = pad_sequences(sequences, padding='post', truncating='post')
+  sequences_array.append(sequences)
+
+  #word_count = len(word_index)
+  len_input = len(sequences[0])
+  lengths_input.append(len_input)
+  if i == 0: # General
+    ohe = LabelBinarizer()
+    labels_transformed_general = ohe.fit_transform(labels)
+  else: # Feeling
+    ohe2 = LabelBinarizer()
+    labels_transformed_feeling = ohe2.fit_transform(labels)
+  #len_output = len(labels_transformed[0])
 
 app.secret_key='test'
 
