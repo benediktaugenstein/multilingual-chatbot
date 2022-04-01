b6b8754628c0ba7fa18f5c3483acb3955d6b98c5
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-04-01 15:13:24 +0200

    Update myfuncs.py

--- myfuncs.py
+++ myfuncs.py
@@ -1,16 +1,3 @@
-model = keras.models.load_model('/model/general')
-model2 = keras.models.load_model('/model/feeling')
-
-#link_general = '/data/general.csv'
-#link_feeling = '/data/feeling.csv'
-models = [model, model2]
-datasets = [link_general, link_feeling]
-tokenizers = []
-word_indices = []
-sequences_array = []
-lengths_input = []
-labels_transformed_array = []
-
 def helper2(x):
   x = x.upper() + 'lalala'
   return x
