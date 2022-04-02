504f7435288c8245bd560d5b0a4688bebdc673b6
Benedikt <74244646+benediktaugenstein@users.noreply.github.com>
2022-04-02 15:56:26 +0200

    Update myfuncs.py

--- myfuncs.py
+++ myfuncs.py
@@ -6,6 +6,7 @@ def new_input(input_initial, tokenizers, lengths_input, models, ohe, ohe2):
   from subs import sub
   from datetime import datetime, timezone, timedelta
   from tensorflow.keras.preprocessing.sequence import pad_sequences
+  sub()
   for key, value in contractions.items():
     search = key + ' '
     if search in input_initial:
